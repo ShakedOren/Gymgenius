38e534170f38584be52cf348d22a9283e66015a8
Roey Shtern <roeyshtern@gmail.com>
2024-03-15 16:23:01 +0200

    changes

---
+++ Gymgenius/BO/User.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Gymgenius.bo
+{
+    public class User
+    {
+        public required int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public int? Age { get; set; }
+        public string? Email { get; set; }
+
+        [SetsRequiredMembers]
+        public User(int id, string firstName, string lastName, int? age, string? email)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Email = email;
+        }
+        public override string ToString()
+        {
+            return $"User ID: {Id}, User Name: {FirstName} {LastName}, Email: {Email}";
+        }
+    }
+}
---
+++ Gymgenius/Program.cs
@@ -0,0 +1,32 @@
+using Gymgenius.bll;
+using Gymgenius.bo;
+using Gymgenius.dal;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddSingleton<IUserRepository, UserMemoryRepository>();
+builder.Services.AddSingleton<IExerciseRepository, ExerciseMemoryRepository>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
