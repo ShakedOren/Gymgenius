c7de27bc683139cf63db4a0057a9789088618c44
Shaked Oren <shakedoren957@gmail.com>
2024-03-18 00:42:22 +0200

    add exercise to program

---
+++ Gymgenius/Gymgenius/DAL/ExerciseToProgramMemoryRepository.cs
@@ -0,0 +1,35 @@
+﻿using Gymgenius.bo;
+using GymGenius.BO;
+
+namespace GymGenius.DAL
+{
+    public class ExerciseToProgramMemoryRepository : IExerciseToProgramRepository
+    {
+        List<KeyValuePair<TrainingProgram, Exercise>> _exerciseToProgram = new List<KeyValuePair<TrainingProgram, Exercise>>();
+
+        public void AddExerciseToProgram(Exercise exercise, TrainingProgram program)
+        {
+            _exerciseToProgram.Add(new KeyValuePair<TrainingProgram, Exercise> (program, exercise));
+        }
+
+        public void DeleteExerciseFromProgram(Exercise exercise, TrainingProgram program)
+        {
+            KeyValuePair<TrainingProgram, Exercise > pairToRemove = _exerciseToProgram.Find(p =>
+             p.Key.Id == program.Id && p.Value.Name == exercise.Name);
+            _exerciseToProgram.Remove(pairToRemove);
+        }
+
+        public List<Exercise> GetAllExerciseOfProgram(TrainingProgram program)
+        {
+            return _exerciseToProgram
+                .Where(p => p.Key.Id == program.Id)
+                .Select(p => p.Value)
+                .ToList();
+        }
+
+        public bool IsExerciseExistsInProgram(Exercise exercise, TrainingProgram program)
+        {
+            return _exerciseToProgram.Any(p => p.Key.Id == program.Id && p.Value.Name == exercise.Name);
+        }
+    }
+}
