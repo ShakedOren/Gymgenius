7574c2baa01efae3074fee8a406952873dc6c8f0
Roey Shtern <roeyshtern@gmail.com>
2024-03-20 22:08:54 +0200

    change init script

--- Gymgenius/Gymgenius/init/init.sql
+++ Gymgenius/Gymgenius/init/init.sql
@@ -14,23 +14,49 @@ GO
 USE GymGenius;
 GO
 
-CREATE TABLE dbo.Exercise (
-  Name VARCHAR(50) NOT NULL PRIMARY KEY,
-  DateCreated DATETIME DEFAULT (GETDATE()) NOT NULL
-);
+-- If the GymGenius database exists
+IF DB_ID(N'GymGenius') IS NOT NULL
+BEGIN
+    -- Truncate existing tables if they exist and then create them
+    IF NOT EXISTS (SELECT 1 FROM sys.tables WHERE name = 'Exercise' AND schema_id = SCHEMA_ID('dbo'))
+    BEGIN
+        CREATE TABLE dbo.Exercise (
+          Name VARCHAR(50) NOT NULL PRIMARY KEY,
+          DateCreated DATETIME DEFAULT (GETDATE()) NOT NULL
+        );
+    END
+    ELSE
+    BEGIN
+        TRUNCATE TABLE dbo.Exercise;
+    END
 
-CREATE TABLE dbo.[Users] (
-  Id INT PRIMARY KEY NOT NULL,
-  DateCreated DATETIME DEFAULT (GETDATE()) NOT NULL,
-  FirstName VARCHAR(50) NOT NULL,
-  LastName VARCHAR(50) NOT NULL,
-  Age INT NULL,
-  Email VARCHAR(255) NULL
-);
+    IF NOT EXISTS (SELECT 1 FROM sys.tables WHERE name = 'Users' AND schema_id = SCHEMA_ID('dbo'))
+    BEGIN
+        CREATE TABLE dbo.Users (
+          Id INT PRIMARY KEY NOT NULL,
+          DateCreated DATETIME DEFAULT (GETDATE()) NOT NULL,
+          FirstName VARCHAR(50) NOT NULL,
+          LastName VARCHAR(50) NOT NULL,
+          Age INT,
+          Email VARCHAR(255)
+        );
+    END
+    ELSE
+    BEGIN
+        TRUNCATE TABLE dbo.Users;
+    END
 
-CREATE TABLE dbo.TrainingProgram (
-  Id INT PRIMARY KEY NOT NULL,
-  DateCreated DATETIME DEFAULT (GETDATE()) NOT NULL,
-  Name NVARCHAR(255) NOT NULL,
-  Description NVARCHAR(max) NULL
-);
+    IF NOT EXISTS (SELECT 1 FROM sys.tables WHERE name = 'TrainingProgram' AND schema_id = SCHEMA_ID('dbo'))
+    BEGIN
+        CREATE TABLE dbo.TrainingProgram (
+          Id INT PRIMARY KEY NOT NULL,
+          DateCreated DATETIME DEFAULT (GETDATE()) NOT NULL,
+          Name NVARCHAR(255) NOT NULL,
+          Description NVARCHAR(MAX)
+        );
+    END
+    ELSE
+    BEGIN
+        TRUNCATE TABLE dbo.TrainingProgram;
+    END
+END
