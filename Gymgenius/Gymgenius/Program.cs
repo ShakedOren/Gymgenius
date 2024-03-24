e6ccfd7f86819e17a16bc7a2e09320bde25c2133
Shaked Oren <shakedoren957@gmail.com>
2024-03-24 21:07:09 +0200

    works with memory

--- Gymgenius/Gymgenius/Program.cs
+++ Gymgenius/Gymgenius/Program.cs
@@ -18,11 +18,18 @@ builder.Services.AddSingleton<ExerciseToProgramManagment>();
 builder.Services.AddSingleton<UserToProgramManagment>();
 
 //builder.Services.AddSingleton<IUserRepository, UserMemoryRepository>();
-builder.Services.AddSingleton<IUserRepository, UserMSSQLRepository>();
-builder.Services.AddSingleton<IExerciseRepository, ExerciseMSSQLRepository>();
-builder.Services.AddSingleton<ITrainingProgramRepository, TrainingProgramMSSQLRepository>();
-builder.Services.AddSingleton<IExerciseToProgramRepository, ExerciseToProgramMSSQLRepository>();
-builder.Services.AddSingleton<IUserToProgramRepository, UserToProgramMSSQLRepository>();
+builder.Services.AddSingleton<IUserRepository, UserMemoryRepository>();
+builder.Services.AddSingleton<IExerciseRepository, ExerciseMemoryRepository>();
+builder.Services.AddSingleton<ITrainingProgramRepository, TrainingProgramMemoryRepository>();
+builder.Services.AddSingleton<IExerciseToProgramRepository, ExerciseToProgramMemoryRepository>();
+builder.Services.AddSingleton<IUserToProgramRepository, UserToProgramMemoryRepository>();
+
+//builder.Services.AddSingleton<IUserRepository, UserMSSQLRepository>();
+//builder.Services.AddSingleton<IExerciseRepository, ExerciseMSSQLRepository>();
+//builder.Services.AddSingleton<ITrainingProgramRepository, TrainingProgramMSSQLRepository>();
+//builder.Services.AddSingleton<IExerciseToProgramRepository, ExerciseToProgramMSSQLRepository>();
+//builder.Services.AddSingleton<IUserToProgramRepository, UserToProgramMSSQLRepository>();
+
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
