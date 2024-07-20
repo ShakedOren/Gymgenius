90d20dbb27c1dc37eee4a7acfd25c0e86960567e
shaked <shakedoren957@gmail.com>
2024-07-20 16:32:59 +0300

    add user view

--- Gymgenius/Gymgenius/BO/User.cs
+++ Gymgenius/Gymgenius/BO/User.cs
@@ -1,16 +1,25 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Gymgenius.bo
 {
     public class User
     {
+        [Required(ErrorMessage = "Username is required")]
         public string UserName { get; set; }
+        [Required(ErrorMessage = "First name is required")]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last name is required")]
         public string LastName { get; set; }
-        public int? Age { get; set; }
+        [Range(1, 100, ErrorMessage = "Age must be between 1 and 100")]
+        public int Age { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
         public string? Email { get; set; }
         public bool IsTrainer{ get; set; }
+        [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
+        [Range(1, 2, ErrorMessage = "RoleId must be between 1 and 2")]
         public int RoleId { get; set; }
 
         public override bool Equals(object? obj)
